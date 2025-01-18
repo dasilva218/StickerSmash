de75bc68b967b1aff280e3eb8eba4ec14877b894
dasilva218 <dasilvawil218@gmail.com>
2025-01-18 12:36:59 +0100

    version2

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,38 @@
-import { View, StyleSheet } from 'react-native';
-import { Image } from 'expo-image';
+import Button from '@/components/Button';
+import ImageViewer from '@/components/ImageWiever';
+import { StyleSheet, View } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import { useState } from 'react';
 
 
-const PlaceholderImage = require('@/assets/images/react-logo@2x.png');
+const PlaceholderImage = require('@/assets/images/adaptive-icon.png');
 
 
 export default function Index() {
+  const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined);
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ['images'],
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri);
+    } else {
+      alert('You did not select any image.');
+    }
+  };
+
+
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <Image source={PlaceholderImage} style={styles.image} />
+        <ImageViewer imgSource={PlaceholderImage} selectedImage={selectedImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button theme="primary" label="Choose a photo" onPress={pickImageAsync} />
+        <Button label="Use this photo" />
       </View>
     </View>
   );
@@ -28,5 +51,9 @@ const styles = StyleSheet.create({
     width: 320,
     height: 440,
     borderRadius: 18,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
   },
 });
--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -3,14 +3,14 @@ import { Link, Stack } from 'expo-router';
 
 export default function NotFoundScreen() {
   return (
-    <p>
+    <div>
       <Stack.Screen options={{ title: 'Oops! Not Found' }} />
       <View style={styles.container}>
         <Link href="/" style={styles.button}>
           Go back to Home screen!
         </Link>
       </View>
-    </p>
+    </div>
   );
 }
 
